84e312f4fd17b4d19861e4f652a54983db0b78ae
maheshpcse <maheshmahi1599@gmail.com>
2022-05-24 11:48:57 +0530

    created srms ui and added admin sidemenu screen design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AdminSidemenuComponent } from './admin/admin-sidemenu/admin-sidemenu.component';
+import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
+import { AdminFooterComponent } from './admin/admin-footer/admin-footer.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        AdminSidemenuComponent,
+        AdminHeaderComponent,
+        AdminFooterComponent,
+        AdminHomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
